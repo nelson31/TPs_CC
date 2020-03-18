3c6872ffd64355fb74c1e3b211c9674a55b2e85f
guel99 <49151121+guel99@users.noreply.github.com>
2020-03-18 10:03:36 +0000

    Atualização.

--- AnonGW/src/AnonGW.java
+++ AnonGW/src/AnonGW.java
@@ -1,4 +1,34 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class AnonGW {
 
+    /**
+     * Socket TCP através do qual o anonGW
+     * aceita conexões provenientes de
+     * clientes que se pretendem anonimizar
+     * para comunicar com um dado servidor
+     */
+    private ServerSocket listen;
 
+    /**
+     * Socket que permite receber dados
+     * de um cliente
+     */
+    private Socket input;
+
+    /**
+     * Socket que será usado para comunicar
+     * com o servidor. (Apenas nesta primeira fase)
+     */
+    private Socket output;
+
+    public static void main(String[] args){
+
+        /* Colocamos o server socket
+        permanentemente à escuta*/
+
+        System.exit(0);
+
+    }
 }
---
+++ AnonGW/src/Worker.java
@@ -0,0 +1,10 @@
+import java.net.Socket;
+
+public class Worker {
+
+    /**
+     * Socket a partir do qual cada
+     * worker receberá dados
+     */
+    private Socket input;
+}
