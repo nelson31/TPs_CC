14a9f92338ccb7aa29cf4102aac005173cbaeab1
guel99 <49151121+guel99@users.noreply.github.com>
2020-04-25 14:05:32 +0100

    Atualização

--- TP2/AnonGW/src/AnonGW.java
+++ TP2/AnonGW/src/AnonGW.java
@@ -35,13 +35,13 @@ public class AnonGW {
             /* Colocamos o servidor à escuta
             na porta 80 */
             listen = new ServerSocket(80);
-            String ipadd = args[2], port = args[4];
+            String ipadd = args[1], port = args[3];
             System.out.println("I'm protecting you to access to " + ipadd + ", port " + port);
             /* Colocamos o server socket
             permanentemente à escuta*/
             while (true) {
                 System.out.println("I'm listening for new requests");
-                Worker w = new Worker(listen.accept(), args[2], args[4]);
+                Worker w = new Worker(listen.accept(), ipadd, port);
                 Thread t = new Thread(w);
                 t.start();
                 System.out.println("New request");
