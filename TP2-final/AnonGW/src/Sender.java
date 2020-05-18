38344dc594e0aef6519c1817dffb7c51bf871f7b
guel99 <49151121+guel99@users.noreply.github.com>
2020-05-18 15:23:36 +0100

    Atualização

--- TP2-final/AnonGW/src/Sender.java
+++ TP2-final/AnonGW/src/Sender.java
@@ -6,21 +6,19 @@ public class Sender {
 
     public static void main(String[] args){
 
-        while(true){
-            try {
-                AnonSocket asocket = new AnonSocket(6666, args[1], new ForeignSessions(), new SessionGetter());
+        try {
+            AnonSocket asocket = new AnonSocket(6666, args[1], new ForeignSessions(), new SessionGetter());
+            System.out.println("Mensagem para enviar:");
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            String message = null;
+            while ((message = bf.readLine()) != null) {
+                byte[] data = message.getBytes();
+                asocket.send(0, data, args[2], args[1], 0);
                 System.out.println("Mensagem para enviar:");
-                BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-                String message = null;
-                while ((message = bf.readLine()) != null) {
-                    byte[] data = message.getBytes();
-                    asocket.send(0, data, args[2], args[1], 0);
-                    System.out.println("Mensagem para enviar:");
-                }
             }
-            catch(IOException exc){
-                System.out.println("Erro ao ler pedido");
-            }
+        }
+        catch(IOException exc){
+            System.out.println("Erro ao ler pedido - " + exc.getMessage());
         }
     }
 }
