1cc92126dce5c19495493395d8288a9d68bd72d4
guel99 <49151121+guel99@users.noreply.github.com>
2020-05-21 19:59:17 +0100

    Atualização

--- Project/src/Components/ForeignSessions.java
+++ Project/src/Components/ForeignSessions.java
@@ -130,6 +130,7 @@ public class ForeignSessions {
             novoId = this.sessionGetter.getID();
             /* Atribuimos logo um id à sessão no anon local */
             this.association.put(data,novoId);
+            System.out.println("Tamanho da colecao: " + association.size());
             System.out.println(this.association.get(ndata));
             System.out.println("[Equals]" + (data.equals(ndata) + "; " + ndata.getOwnerIP().toString() + "/"
                     + data.getId() + "; " + ndata.getId()));
