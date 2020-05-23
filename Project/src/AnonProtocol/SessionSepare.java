5a757236fc8df7ea6f7a4b82531508229d51fc4b
guel99 <49151121+guel99@users.noreply.github.com>
2020-05-23 12:07:00 +0100

    Atualização

--- Project/src/AnonProtocol/SessionSepare.java
+++ Project/src/AnonProtocol/SessionSepare.java
@@ -89,7 +89,7 @@ public class SessionSepare implements Runnable {
                 /* Adicionamos o pacote à tabela */
                 this.incoming.addPacket(ap,ap.getOwnerIP());
                 if(ap.getPayloadSize() == -1)
-                    System.out.println("[Separe] Recebi pacote de fecho");
+                    System.out.println("[Separe] Recebi pacote de fecho com seq: " + ap.getSequence());
             }
             catch(UnknownHostException exc){
                 System.out.println("Host inexistente");
